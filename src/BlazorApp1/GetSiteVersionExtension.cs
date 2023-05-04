1234b3556ee30a33196474bc3e77f5e741361d08
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2023-05-04 12:51:08 +0300

    Added Unit tests

---
+++ src/BlazorApp1/GetSiteVersionExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace BlazorApp1
+{
+    public static class GetSiteVersionExtension
+    {
+        public static Version GetSiteVersion()
+        {
+            Version version = Assembly.GetExecutingAssembly()?.GetName().Version ?? new Version(1, 0, 0, 0);
+            return version;
+        }
+    }
+}
---
+++ src/BlazorApp1TestProject/Unit/GetSiteVersionExtensionTests.cs
@@ -0,0 +1,15 @@
+﻿using BlazorApp1;
+
+namespace BlazorApp1TestProject.Unit
+{
+    [TestClass, TestCategory("Unit")]
+    public class GetSiteVersionExtensionTests
+    {
+        [TestMethod()]
+        public void GetSiteVersionTest()
+        {
+            var version = GetSiteVersionExtension.GetSiteVersion();
+            Assert.AreEqual(version, new Version(1, 0, 0, 0));
+        }
+    }
+}
